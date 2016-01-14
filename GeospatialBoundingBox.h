7fb852f7a7c90a128533c9e0aa506d880784d45c
Charalambos Poullis <charalambos@poullis.org>
2016-01-14 10:33:35 -0500

    minor change

--- GeospatialBoundingBox.h
+++ GeospatialBoundingBox.h
@@ -96,8 +96,7 @@ class GeospatialBoundingBox : public BoundingBox	{
 		void cleanUp();
 
 		///Resample into the right-sized XYZ in order to minimize information loss
-		bool resample(double sampling_tolerance= EPSILON, double 
-step = 0.05);
+		bool resample(double sampling_tolerance= 1.0, double step = 0.05);
 
 		///Loads the information from disk
 		void load(int index, std::string *base_file_name=0x00);
